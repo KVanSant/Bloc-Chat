9d409238fc19ca5815dfa049e1f17e092ca4340b
Davis Van Sant <davisvansant@thelostwayfarer-imac.local>
2015-02-13 15:13:01 -0800

    modal progress thus far

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,5 @@
-blocchat = angular.module('BlocChat', ['firebase', 'ui.router']);
+blocchat = angular.module('BlocChat', ['firebase', 'ui.router', 'ui.bootstrap']);
+
 
 blocchat.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
@@ -8,10 +9,46 @@ blocchat.config(['$stateProvider', '$locationProvider', function($stateProvider,
     controller: 'Home.controller',
     templateUrl: '/templates/home.html'
   });
+}]);
 
 
-blocchat.controller('Home.controller', ['$scope', '$firebase',  function($scope, $firebase) {
-  var ref = new Firebase("https://blocchat.firebaseio.com/");
-  var sync = $firebase(ref);
+
+blocchat.controller('Home.controller', ['$scope', '$firebase', 'Room', function($scope, $firebase, $modal, Room) {
+  $scope.room = Rooms; 
+  
+  $scope.open = function(size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: '/templates/modal.html',
+      controller: 'ModalInstanceCtrl',
+      size: size
+    })
+  };
 
 }]);
+
+blocchat.controller('ModalInstance',['$scope', '$firebase', function($scope, $firebase) {
+
+
+
+
+}]);
+
+
+
+blocchat.factory('Room', ['$firebase', function($firebase) {
+  var ref = new Firebase("https://blocchat.firebaseio.com/");
+  var rooms = $firebase(ref.child('rooms')).$asArray();
+
+  addRoom = function(room) {
+    rooms.$add({room: room});
+    newRoom = "";
+  };
+
+  return {
+    all: rooms
+  }
+}]);
+
+
+
